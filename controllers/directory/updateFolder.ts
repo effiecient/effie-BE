8f3d1f01f85891405d8542934a1411f4c5cce81d
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-03-24 01:02:19 +0700

    wip: updateFolder. tested on basic case, seems fine

--- controllers/directory/updateFolder.ts
+++ controllers/directory/updateFolder.ts
@@ -2,23 +2,17 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { STATUS_SUCCESS, STATUS_ERROR } from "../../config";
 import utils from "../../utils";
 
-//   example data
-// const username = "christojeffrey";
-
-// const path = "/";
-// const relativePath = "searchEngines";
-// const title = "Search Engines";
-// const isPinned = false;
-
-// MUST HAVE USERNAME AND PATH
-
-// example data
-// const body = {
-//     username: "christojeffrey",
-//     path: "/",
-//     relativePath: "a",
-//     isPinned: false,
-// }
+// example of complete data
+const body = {
+  // MUST HAVE USERNAME PATH and RELATIVE PATH
+  username: "christojeffrey",
+  path: "/",
+  relativePath: "a",
+  // data below is optional. at least one of them must be provided
+  isPinned: false,
+  title: "A",
+  newRelativePath: "b",
+};
 
 export async function updateFolder(req: VercelRequest, res: VercelResponse) {
   // const { username, path, relativePath, title, isPinned } = req.body;
@@ -26,8 +20,11 @@ export async function updateFolder(req: VercelRequest, res: VercelResponse) {
   // temporary data
   const username = "christojeffrey";
   const path = "/itb";
-  const relativePath = "semester-6";
-  const isPinned = true;
+  const relativePath = "semester-4";
+  const isPinned = false;
+  const title = "Semester 5";
+  const newRelativePath = "semester-5";
+
   if (!username || !path || !relativePath) {
     res.status(400).json({
       status: STATUS_ERROR,
@@ -50,53 +47,85 @@ export async function updateFolder(req: VercelRequest, res: VercelResponse) {
   // turn path from "/" or "/testing"or "/testing/another" ["testing", "another"]
   let pathArray = path.split("/").filter((item: any) => item !== "");
   // append relative path to pathArray
+  console.log(pathArray);
 
   const directoryRootRef = db.collection("directories").doc(username);
   let parentRef = directoryRootRef;
+
   // get parent
   for (let i = 0; i < pathArray.length; i++) {
     const pathItem = pathArray[i];
     const childRef = parentRef.collection("childrens").doc(pathItem);
-    const childData = await childRef.get();
-    console.log(childData.data());
-    if (!childData.exists) {
-      // if folder doesn't exist, break and return error
-      res.status(404).json({
+    parentRef = childRef;
+  }
+  let parentData = await parentRef.get();
+  if (!parentData.exists) {
+    // if folder doesn't exist, break and return error
+    res.status(404).json({
+      status: STATUS_ERROR,
+      message: "Parent not found.",
+    });
+    return;
+  }
+  // get the folder
+  let folderRef = parentRef.collection("childrens").doc(relativePath);
+  let folderData = await folderRef.get();
+  if (!folderData.exists) {
+    res.status(404).json({
+      status: STATUS_ERROR,
+      message: "Folder not found.",
+    });
+    return;
+  }
+
+  // at this point, we have valid parent and folder
+
+  folderData = folderData.data();
+  parentData = parentData.data();
+
+  // 1. update the folder
+  let updatedFolderData = {
+    ...folderData,
+    isPinned: isPinned ? isPinned : folderData.isPinned,
+    title: title ? title : folderData.title,
+  };
+  await folderRef.update(updatedFolderData, { merge: true });
+
+  // handle new relative path
+  if (newRelativePath) {
+    // check if new relative path is already exist
+    const newFolderRef = parentRef.collection("childrens").doc(newRelativePath);
+    const newFolderData = await newFolderRef.get();
+    if (newFolderData.exists) {
+      res.status(400).json({
         status: STATUS_ERROR,
-        message: "Folder not found",
+        message: "Folder already exist.",
       });
       return;
     }
-    parentRef = childRef;
+    // update the folder by deleting the old one and creating a new one
+    await folderRef.delete();
+    await newFolderRef.set(updatedFolderData);
   }
 
-  // parentRef is now the folder to be updated
-  let folderData = await parentRef.get();
-  folderData = folderData.data();
+  // 2. update the parent. childrens is an object with relativePath as key and data as value
+  let updatedChildren = parentData.childrens;
+  // delete the old relative path
+  delete updatedChildren[relativePath];
+  // add the new relative path
+  delete updatedFolderData.childrens;
+  updatedChildren[newRelativePath ? newRelativePath : relativePath] = {
+    ...updatedFolderData,
+  };
+  const updatedParentData = {
+    ...parentData,
+    childrens: updatedChildren,
+  };
 
-  console.log(folderData);
-
-  // add the folder to the parent
-  // const folderData = {
-  //   title: body.title,
-  //   isPinned,
-  //   type: "folder",
-  // };
-
-  // update the parent
-  await parentRef.update(folderData, { merge: true });
-
-  // create a new documents inside the childrens collection
-  // const folderRef = parentRef.collection("childrens").doc(relativePath);
-
-  // await folderRef.set({
-  //   title,
-  //   isPinned,
-  //   type: "folder",
-  // });
+  await parentRef.update(updatedParentData, { merge: true });
 
   res.status(200).json({
     status: STATUS_SUCCESS,
-    data: folderData,
+    data: updatedFolderData,
   });
 }
